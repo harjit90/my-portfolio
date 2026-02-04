b610938627968e4deefa1548ca18f85221bfb85e
harjit90 <mehmi.harjit@gmail.com>
2026-02-04 12:23:27 +0530

    update code for config file

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+// import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom' // used to work it for github pages
 import App from './App.jsx'
 import { MyThemeContext } from './context/ThemeContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter>
     <MyThemeContext>
       <App />
     </MyThemeContext>
-  </BrowserRouter>,
+  </HashRouter>,
 )
