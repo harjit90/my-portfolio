26c4e53c44041ec719535b8e9021b9ca429f7e9b
harjit90 <mehmi.harjit@gmail.com>
2026-02-04 12:06:30 +0530

    update code for config file

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: mode === 'github' ? '/my-portfolio/' : '/',
-}))
+  base: '/my-portfolio/',
+})
